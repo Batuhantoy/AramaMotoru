68822303aa91bbb9cf1570aabe8053555b88f262
Batuhantoy <88581656+Batuhantoy@users.noreply.github.com>
2021-12-08 14:46:10 +0300

    Add files via upload
    C# ile Arama Motoru

---
+++ AramaMotoru/AramaMotoru/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace AramaMotoru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(347, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(699, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(92, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Google\'da Ara";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(699, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Yandex\'te Ara";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(13, 76);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(680, 323);
+            this.webBrowser1.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(724, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(169, 147);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(500, 42);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 29);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Favorilere Ekle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.Location = new System.Drawing.Point(590, 42);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 28);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Favorilerden Kaldır";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "https://www.facebook.com.tr"});
+            this.listBox2.Location = new System.Drawing.Point(724, 241);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(169, 134);
+            this.listBox2.TabIndex = 12;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(818, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Geçmiş";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::AramaMotoru.Properties.Resources.temizle;
+            this.button5.Location = new System.Drawing.Point(638, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(43, 30);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::AramaMotoru.Properties.Resources.reflesh;
+            this.button4.Location = new System.Drawing.Point(569, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(43, 30);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::AramaMotoru.Properties.Resources.ab2;
+            this.button3.Location = new System.Drawing.Point(500, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(43, 30);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::AramaMotoru.Properties.Resources.ileri;
+            this.button2.Location = new System.Drawing.Point(69, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 30);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::AramaMotoru.Properties.Resources.geri1;
+            this.button1.Location = new System.Drawing.Point(13, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 30);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(69, 126);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(543, 238);
+            this.listBox3.TabIndex = 14;
+            this.listBox3.Visible = false;
+            this.listBox3.DoubleClick += new System.EventHandler(this.listBox3_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(905, 402);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Arama Motoru";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ AramaMotoru/AramaMotoru/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AramaMotoru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string link;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                if (listBox2.Items[i].ToString().IndexOf(textBox1.Text) > -1)
+                {
+                    if (radioButton1.Checked)
+                    {
+                        MessageBox.Show("Bu  Siteye Google ile Giriş İzniniz Yok");
+                        textBox1.ResetText();
+                        radioButton1.Checked = false;
+                        radioButton2.Checked = false;
+                    }
+                    if (radioButton2.Checked)
+                    {
+                        MessageBox.Show("Bu  Siteye Yandex ile Giriş İzniniz Yok");
+                        textBox1.ResetText();
+                        radioButton1.Checked = false;
+                        radioButton2.Checked = false;
+                    }
+                }
+                else
+                {
+                    if (radioButton1.Checked)
+                    {
+                        link = "https://www.google.com/search?q=" + textBox1.Text;
+                        webBrowser1.Navigate(link);
+                        listBox3.Items.Add(link);
+                    }
+                    if (radioButton2.Checked)
+                    {
+                        link = "https://www.yandex.com/search/?text=" + textBox1.Text;
+                        webBrowser1.Navigate(link);
+                        listBox3.Items.Add(link);
+                    }
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.DocumentText = "";
+            textBox1.ResetText();
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(webBrowser1.Url);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(listBox1.SelectedItem.ToString());
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                for (int i = 0; i < listBox2.Items.Count; i++)
+                {
+                    if (listBox2.Items[i].ToString().IndexOf(textBox1.Text) > -1)
+                    {
+                        if (radioButton1.Checked)
+                        {
+                            MessageBox.Show("Bu  Siteye Google ile Giriş İzniniz Yok");
+                            textBox1.ResetText();
+                            radioButton1.Checked = false;
+                            radioButton2.Checked = false;
+                        }
+                        if (radioButton2.Checked)
+                        {
+                            MessageBox.Show("Bu  Siteye Yandex ile Giriş İzniniz Yok");
+                            textBox1.ResetText();
+                            radioButton1.Checked = false;
+                            radioButton2.Checked = false;
+                        }
+                    }
+                    else
+                    {
+                        if (radioButton1.Checked)
+                        {
+                            link = "https://www.google.com/search?q=" + textBox1.Text;
+                            webBrowser1.Navigate(link);
+                            listBox3.Items.Add(link);
+                        }
+                        if (radioButton2.Checked)
+                        {
+                            link = "https://www.yandex.com/search/?text=" + textBox1.Text;
+                            webBrowser1.Navigate(link);
+                            listBox3.Items.Add(link);
+                        }
+                    }
+                }
+            }
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (listBox3.Visible == false)
+            {
+                listBox3.Visible = true;
+            }
+            else
+            {
+                listBox3.Visible = false;
+            }                    
+        }
+
+        private void listBox3_DoubleClick(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(listBox3.SelectedItem.ToString());
+            listBox3.Visible = false;
+        }
+    }
+}
